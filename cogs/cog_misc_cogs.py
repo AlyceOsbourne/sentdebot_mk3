4c7f73da4d9f9f98ccaf71b61e7de1a89c6dbb4d
Alyce <Alyceosbourne@outlook.com>
2022-07-09 21:34:40 +0100

    fixed the blasted thing

--- cogs/cog_misc_cogs.py
+++ cogs/cog_misc_cogs.py
@@ -1,3 +1,4 @@
+import nextcord
 from nextcord.ext import commands
 
 
@@ -5,18 +6,23 @@ class MiscCogs(commands.Cog, name="Misc Cogs"):
     """Cog that holds random commands"""
     def __init__(self, bot):
         self.bot = bot
+        # delete the help command
+        self.bot.remove_command('help')
 
     @commands.command(name="membercount", help="Get the member count of a guild")
     async def membercount(self, ctx):
         await ctx.send(f"{ctx.guild.member_count}")
 
-    @commands.command(name="commands", help="Get all registered commands")
+    @commands.command(name="commands", aliases=["help"], help="Get all registered commands")
     async def commands(self, ctx):
         c = []
         for command in self.bot.commands:
-            if not command.hidden:
-                c.append(command.name)
-        await ctx.send("return {" + ", ".join(f'"{command}": {command.help}' for command in c) + "}")
+            c.append((command.name, command.help))
+        c.sort(key=lambda x: x[0])
+        # format like python dict return
+        await ctx.send("return {\n\t" + ",\n\t".join(f"{k}: {v}" for k, v in c) + "\n}")
+
+
 
 
 def setup(bot):
