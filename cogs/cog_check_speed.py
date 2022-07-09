adf57173955575ed8d4da354a56890ee1ec73cc8
Alyce <Alyceosbourne@outlook.com>
2022-07-09 21:22:18 +0100

    Attempting to find bug that causes commands to not work

---
+++ cogs/cog_check_speed.py
@@ -0,0 +1,17 @@
+"""Cog that simply checks the speed of the bot"""
+
+from nextcord.ext import commands
+
+
+class CheckSpeed(commands.Cog, name="Speed Test"):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='check_latency', help='check the speed of the bot')
+    async def check_latency(self, ctx):
+        await ctx.send(f'```py\n {round(self.bot.latency * 1000)}ms```')
+
+
+
+def setup(bot):
+    bot.add_cog(CheckSpeed(bot))
--- sentdebot.py
+++ sentdebot.py
@@ -1,3 +1,4 @@
+import nextcord
 from nextcord.ext import commands
 import os
 from dotenv import load_dotenv
@@ -10,17 +11,43 @@ bot_version = f"v{'.'.join(map(str, __version__))}"
 
 class Sentdebot(commands.Bot):
     def __init__(self, **options):
-        super().__init__(**options)
+        super().__init__(command_prefix="sentdebot.", strip_after_prefix=True, **options)
         self.token = os.environ.get('TOKEN')
 
     def run(self):
+        if not os.path.exists('cogs'):
+            os.makedirs('cogs')
+        for file in os.listdir('cogs'):
+            if file.startswith('cog_'):
+                try:
+                    if os.path.isdir(os.path.join('cogs', file)):
+                        for init_file in os.listdir(os.path.join('cogs', file)):
+                            if init_file.startswith('__init__'):
+                                self.load_extension(f'cogs.{file}.{init_file[:-3]}')
+                    elif file.endswith('.py'):
+                        self.load_extension(f'cogs.{file[:-3]}')
+                except Exception as e:
+                    print(f'Error loading cog: {e}')
+                    continue
         super().run(self.token, reconnect=True)
 
     @commands.Cog.listener()
     async def on_ready(self):
         for guild in self.guilds:
-            for channel in guild.text_channels:
-                await channel.send(f"Sentdebot {bot_version} is ready!")
+            await guild.system_channel.send(f'Sentdebot {bot_version} is online!')
+            # make an embed witha list of loaded cogs
+            embed = nextcord.Embed(title='Loaded cogs', color=0x00ff00)
+            for cog in self.cogs:
+                embed.add_field(name=cog, value=self.get_cog(cog).__doc__)
+            await guild.system_channel.send(embed=embed)
+
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+
+
 
 
 if __name__ == "__main__":
